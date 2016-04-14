e2a0b8f62dc1cd4ba7314afd24eebde1290c157e
Martin Miranda <martinmirandac@gmail.com>
2016-04-14 20:48:33 +1000

    added bootstrap (#9)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,7 +20,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
+        format.html { redirect_to recipes_url, notice: 'Recipe was successfully created.' }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new }
@@ -32,7 +32,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
+        format.html { redirect_to recipes_url, notice: 'Recipe was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit }
